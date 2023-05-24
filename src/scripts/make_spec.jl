d9d050414c213a918b0cee6b3281ba1be3589d53
Michael Palumbo <palumbomi95@gmail.com>
2023-05-24 12:16:12 -0400

    deps + mplstyle

--- src/scripts/make_spec.jl
+++ src/scripts/make_spec.jl
@@ -15,20 +15,20 @@ mpl.style.use(GRASS.moddir * "fig.mplstyle")
 
 # set up paramaters for spectrum
 N = 132
-Nt = 50
-lines = [5250.6, 5434.5, 6173.4]
-depths = [0.8, 0.9, 0.6]
-geffs = [0.0, 0.0, 0.0]
-templates = ["FeI_5250.6", "FeI_5434", "FeI_6173"]
-variability = repeat([true], length(lines))
+Nt = 10
+lines = [5250.6]#, 5434.5, 6173.4]
+depths = [0.8]#, 0.9, 0.6]
+geffs = [0.0]#, 0.0, 0.0]
+templates = ["FeI_5250.6"]#, "FeI_5434", "FeI_6173"]
+variability = trues(length(lines))
 resolution = 7e5
 seed_rng = true
-use_gpu = true
+use_gpu = CUDA.functional()
 
 disk = DiskParams(N=N, Nt=Nt)
 spec1 = SpecParams(lines=lines, depths=depths, variability=variability,
                    geffs=geffs, templates=templates, resolution=resolution)
-lambdas1, outspec1 = synthesize_spectra(spec1, disk, seed_rng=seed_rng, verbose=true, use_gpu=use_gpu)
+wavs, flux = synthesize_spectra(spec1, disk, seed_rng=seed_rng, verbose=true, use_gpu=use_gpu)
 
-df = DataFrame("wave" => lambdas1, "flux" => dropdims(mean(outspec1, dims=2), dims=2))
-CSV.write("/storage/work/m/mlp95/GRASS/test_spec.csv", df)
+plt.plot(wavs, flux)
+plt.show()
