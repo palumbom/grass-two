fd9f5f4250313b404e6a6d9b07d93d5221c5d1b2
Michael Palumbo <palumbomi95@gmail.com>
2023-07-30 11:50:17 -0400

    Picket fence with more lines

--- src/scripts/noise_scaling_plot.jl
+++ src/scripts/noise_scaling_plot.jl
@@ -18,7 +18,11 @@ colors = ["#56B4E9", "#E69F00", "#009E73", "#CC79A7"]
 
 # get command line args and output directories
 include(joinpath(abspath(@__DIR__), "paths.jl"))
-const datafile = string(abspath(joinpath(data, "spectra_for_bin.jld2")))
+const datafile = string(abspath(joinpath(data, "picket_fence.jld2")))
+plotsubdir = joinpath(figures, "snr_plots")
+if !isdir(plotsubdir)
+    mkdir(plotsubdir)
+end
 
 # get line properties
 lp = GRASS.LineProperties()
@@ -31,61 +35,51 @@ templates = d["templates"]
 lines = d["lines"]
 depths = d["depths"]
 
-# collect lines
-lines = collect(lines)
+# get separation between lines
+line_sep = mean(diff(sort(lines)))
 
-# isolate a chunk of spectrum around a good line
-idx = findall(x -> occursin.("FeI_5576", x), templates)
-line_centers = sort!(lines[idx])
-line_center = line_centers[5]
-
-buffer = 1.0
-idx1 = findlast(x -> x .<= line_center - buffer, wavs)
-idx2 = findlast(x -> x .<= line_center + buffer, wavs)
-
-# take isolated view
-wavs_iso = copy(view(wavs, idx1:idx2))
-flux_iso = copy(view(flux, idx1:idx2, :))
-
-# make noises to loop over
-SNRs = range(100.0, 1000.0, step=10.0)
-
-#=# allocate memory for rvs stuff
-rv_std = zeros(length(SNRs))
-
-# loop over them
-flux_noise = similar(flux_iso)
-for i in eachindex(SNRs)
-    # copy over SNR infinity flux
-    flux_noise .= flux_iso
-
-    # add noise
-    GRASS.add_noise!(flux_noise, SNRs[i])
-
-    # calculate ccf
-    v_grid, ccf1 = calc_ccf(wavs_iso, flux_noise, lines[idx], depths[idx], 7e5)
-    rvs1, sigs1 = calc_rvs_from_ccf(v_grid, ccf1)
-    rv_std[i] = std(rvs1)
+function get_lines(wavs, flux, last_line_number)
+    idx = findfirst(x -> x .>= lines[last_line_number] + 0.5 * line_sep, wavs)
+    return view(wavs, 1:idx), view(flux, 1:idx, :)
 end
 
-plt.plot(SNRs, rv_std)
-plt.show()
-=#
+function get_one_line(wavs, flux, line_number)
+    idx1 = findfirst(x -> x .>= lines[line_number] - 0.5 * line_sep, wavs)
+    idx2 = findfirst(x -> x .>= lines[line_number] + 0.5 * line_sep, wavs)
+    return view(wavs, idx1:idx2), view(flux, idx1:idx2, :)
+end
+
+# TODO do they not match or is this numerical noise????
+#=for i in 1:10
+    # get just line
+    wavs_temp, flux_temp = get_one_line(wavs, flux, i)
+
+    # get bisector
+    bis, int = GRASS.calc_bisector(wavs_temp, flux_temp, nflux=50)
+
+    vel = GRASS.c_ms * (bis .- lines[i]) ./ (lines[i])
+
+    plt.plot(vel, int)
+end
+plt.show()=#
 
 function std_vs_number_of_lines(snr::T) where T<:Float64
-    # get spectrum at specified snr
-    wavs_snr = copy(wavs)
-    flux_snr = copy(flux)
-    GRASS.add_noise!(flux_snr, snr)
-
-    rvs_std = zeros(length(idx))
+    # allocate memory
+    rvs_std = zeros(length(lines))
 
     # include more and more lines in ccf
-    for i in 1:length(idx)
-        @show i
+    for i in 1:length(lines)
         # get lines to include in ccf
-        ls = lines[idx[1:i]]
-        ds = depths[idx[1:i]]
+        ls = lines[1:i]
+        ds = depths[1:i]
+
+        # get view of spectrum
+        wavs_temp, flux_temp = get_lines(wavs, flux, i)
+
+        # get spectrum at specified snr
+        wavs_snr = copy(wavs_temp)
+        flux_snr = copy(flux_temp)
+        GRASS.add_noise!(flux_snr, snr)
 
         # calculate ccf
         v_grid, ccf1 = calc_ccf(wavs_snr, flux_snr, ls, ds, 7e5)
@@ -116,7 +110,7 @@ function std_vs_number_of_lines(snr::T) where T<:Float64
         ax2.set_ylabel(L"{\rm BIS}\ - \overline{\rm BIS}\ {\rm (m\ s}^{-1}{\rm )}")
         ax2.set_title("SNR = " * string(Int(snr)) * ", Nlines = " * string(i))
         ax2.legend()
-        ofile = string(joinpath(figures, "rv_vs_bis_snr_" * string(Int(snr)) * "_lines_" * string(i) * ".pdf"))
+        ofile = string(joinpath(plotsubdir, "rv_vs_bis_snr_" * string(Int(snr)) * "_lines_" * string(i) * ".pdf"))
         fig2.savefig(ofile)
         plt.clf()
     end
@@ -127,17 +121,16 @@ end
 snrs_for_lines = range(100.0, 1000.0, step=100.0)
 
 # set up colors
-pcolors = plt.cm.rainbow(range(0, 1, length=length(idx)))
+pcolors = plt.cm.rainbow(range(0, 1, length=length(lines)))
+rvs_std_out = zeros(length(lines), length(snrs_for_lines))
 
-rvs_std_out = zeros(length(idx), length(snrs_for_lines))
-
+# write the data
+jldsave(joinpath(data, "rvs_std_out.jld2"), snrs_for_lines=snrs_for_lines, rvs_std_out=rvs_std_out)
 
 for i in eachindex(snrs_for_lines)
     # get the stuff
-    derp  = std_vs_number_of_lines(snrs_for_lines[i])
-
-    rvs_std_out[:, i] = derp
-
+    out = std_vs_number_of_lines(snrs_for_lines[i])
+    rvs_std_out[:, i] = out
 end
 
 # set up plots
@@ -145,7 +138,7 @@ fig1, ax1 = plt.subplots()
 
 # plot snr vs number of lines
 for i in eachindex(snrs_for_lines)
-    ax1.plot(1:length(idx), rvs_std_out[:,i], label="SNR = " * string(snrs_for_lines[i]), c=pcolors[i,:])
+    ax1.plot(1:length(lines), rvs_std_out[:,i], label="SNR = " * string(snrs_for_lines[i]), c=pcolors[i,:])
 end
 
 ax1.set_xlabel("Number of lines")
@@ -155,3 +148,55 @@ ax1.set_yscale("log", base=2)
 ax1.legend(loc="center left", bbox_to_anchor=(1, 0.5))
 fig1.savefig(string(joinpath(figures, "std_vs_number_of_lines_same.pdf")))
 plt.clf(); plt.close("all")
+
+
+
+
+
+
+
+
+
+
+
+#=
+# collect lines
+lines = collect(lines)
+
+# isolate a chunk of spectrum around a good line
+idx = findall(x -> occursin.("FeI_5576", x), templates)
+line_centers = sort!(lines[idx])
+line_center = line_centers[5]
+
+buffer = 1.0
+idx1 = findlast(x -> x .<= line_center - buffer, wavs)
+idx2 = findlast(x -> x .<= line_center + buffer, wavs)
+
+# take isolated view
+wavs_iso = copy(view(wavs, idx1:idx2))
+flux_iso = copy(view(flux, idx1:idx2, :))
+
+# make noises to loop over
+SNRs = range(100.0, 1000.0, step=10.0)
+
+# # allocate memory for rvs stuff
+# rv_std = zeros(length(SNRs))
+
+# # loop over them
+# flux_noise = similar(flux_iso)
+# for i in eachindex(SNRs)
+#     # copy over SNR infinity flux
+#     flux_noise .= flux_iso
+
+#     # add noise
+#     GRASS.add_noise!(flux_noise, SNRs[i])
+
+#     # calculate ccf
+#     v_grid, ccf1 = calc_ccf(wavs_iso, flux_noise, lines[idx], depths[idx], 7e5)
+#     rvs1, sigs1 = calc_rvs_from_ccf(v_grid, ccf1)
+#     rv_std[i] = std(rvs1)
+# end
+
+# plt.plot(SNRs, rv_std)
+# plt.show()
+=#
---
+++ src/scripts/shape_shift.jl
@@ -0,0 +1,87 @@
+# imports
+using JLD2
+using CUDA
+using GRASS
+using Printf
+using FileIO
+using Profile
+using Statistics
+using EchelleCCFs
+using BenchmarkTools
+using Polynomials
+
+# plotting
+using LaTeXStrings
+import PyPlot; plt = PyPlot; mpl = plt.matplotlib; plt.ioff()
+mpl.style.use(GRASS.moddir * "fig.mplstyle")
+colors = ["#56B4E9", "#E69F00", "#009E73", "#CC79A7"]
+
+# get command line args and output directories
+include(joinpath(abspath(@__DIR__), "paths.jl"))
+const datafile = string(abspath(joinpath(data, "spectra_for_bin.jld2")))
+
+# get line properties
+lp = GRASS.LineProperties()
+
+# read in the data
+d = load(datafile)
+wavs = d["wavs"]
+flux = d["flux"]
+templates = d["templates"]
+lines = d["lines"]
+depths = d["depths"]
+
+# collect lines
+lines = collect(lines)
+
+# isolate a chunk of spectrum around a good line
+idx = findall(x -> occursin.("CaI_6169.0", x), templates)
+sort_idx = sortperm(lines[idx])
+line_centers = lines[idx][sort_idx]
+the_depths = depths[idx][sort_idx]
+
+line_center = line_centers[5]
+the_depth = the_depths[5]
+
+buffer = 1.0
+idx1 = findlast(x -> x .<= line_center - buffer, wavs)
+idx2 = findlast(x -> x .<= line_center + buffer, wavs)
+
+# take isolated view
+wavs_iso = copy(view(wavs, idx1:idx2))
+flux_iso = copy(view(flux, idx1:idx2, :))
+
+# get bisectors
+bis, int = GRASS.calc_bisector(wavs_iso, flux_iso, nflux=50)
+bis = GRASS.c_ms .* (bis .- line_center) ./ (line_center)
+
+# compute velocities
+v_grid, ccf1 = calc_ccf(wavs_iso, flux_iso, [line_center], [1.0 - minimum(flux_iso)], 7e5)
+rvs1, sigs1 = calc_rvs_from_ccf(v_grid, ccf1)
+
+# subtract off the mean
+rvs1 .-= mean(rvs1)
+
+# get the mean bisector
+mean_bis = mean(bis, dims=2)
+mean_int = mean(int, dims=2)
+
+# get the redshifted bisectors
+blu_idx = rvs1 .< -0.5
+red_idx = rvs1 .> 0.5
+
+# get means for red and blue
+mean_bis_red = mean(bis[:, red_idx], dims=2)
+mean_bis_blu = mean(bis[:, blu_idx], dims=2)
+mean_int_red = mean(int[:, red_idx], dims=2)
+mean_int_blu = mean(int[:, blu_idx], dims=2)
+
+# plot them all
+plt.plot(mean_bis, mean_int, c="k")
+plt.plot(mean_bis_red, mean_int_red, c="red")
+plt.plot(mean_bis_blu, mean_int_blu, c="tab:blue")
+plt.show()
+
+plt.plot(mean_bis .- mean_bis_red, mean_int, c="red", ls="--")
+plt.plot(mean_bis .- mean_bis_blu, mean_int, c="tab:blue", ls="--")
+plt.show()
